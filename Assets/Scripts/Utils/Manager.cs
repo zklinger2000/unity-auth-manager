e84c0d67d9bba545b5dfc4023a838dee60be0831
Zack Klinger <zack@zklinger.com>
2019-03-19 15:19:50 -0700

    class(Manager): Create a Manager class to replace the Singleton which doesn't replace the original, but refuses to create a new one

---
+++ Assets/Scripts/Utils/Manager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public abstract class Manager<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get { return instance; }
+        set
+        {
+            if (null == instance)
+            {
+                instance = value;
+                DontDestroyOnLoad(instance.gameObject);
+            }
+            else if (instance != value)
+            {
+                Destroy(value.gameObject);
+            }
+        }
+    }
+
+    public virtual void Awake()
+    {
+        Instance = this as T;
+    }
+}
