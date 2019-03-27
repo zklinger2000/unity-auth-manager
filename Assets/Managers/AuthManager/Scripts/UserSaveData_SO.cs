3295699f1f285372dbc77f9cc48058d7eeeb1aa6
Zack Klinger <zack@zklinger.com>
2019-03-27 15:02:23 -0700

    manager(Auth): Create a class for saving User data using PlayerPrefs

---
+++ Assets/Managers/AuthManager/Scripts/UserSaveData_SO.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New User Save Data", menuName = "User/Data", order = 1)]
+public class UserSaveData_SO : ScriptableObject
+{
+
+    [SerializeField] private string _username;
+    [SerializeField] private string _token;
+
+    [Header("Save Data")] [SerializeField] private string key;
+
+    public string Username
+    {
+        get { return _username; }
+        set { _username = value; }
+    }
+
+    public string Token
+    {
+        get { return _token; }
+        set { _token = value; }
+    }
+
+    public void Save()
+    {
+        if (key == "")
+        {
+            key = name;
+        }
+        string jsonData = JsonUtility.ToJson(this, true);
+        PlayerPrefs.SetString(key, jsonData);
+        PlayerPrefs.Save();
+    }
+
+    public void Load()
+    {
+        JsonUtility.FromJsonOverwrite(PlayerPrefs.GetString(key), this);
+    }
+}
